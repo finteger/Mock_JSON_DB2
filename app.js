49cf04a8fd085883c501c52cd39048681e8a8af7
finteger <nashassociatesinc@gmail.com>
2024-11-21 10:34:24 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const ejs = require('ejs');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.static('public'));
+
+app.listen(PORT, ()=>{
+    console.log(`Connected to port ${port}`);
+});
